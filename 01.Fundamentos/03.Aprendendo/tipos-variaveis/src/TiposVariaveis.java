f2f6e13d1b532776101ddc182057165e7935bde7
Gabriel Moura Martins da Silva <mmartins.gabriel@yahoo.com>
2023-02-11 19:52:28 -0300

    Tipos de Variaveis

--- 01.Fundamentos/03.Aprendendo/tipos-variaveis/src/TiposVariaveis.java
+++ 01.Fundamentos/03.Aprendendo/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,32 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        /*Exemplo Variáveis */
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        /*Exemplo Constante */
+
+        final double VALOR_DE_PI = 3.14;
+
+        VALOR_DE_PI = 10.75;
+
+        /*Constantes devem ter sempre "final" escrito no inicio e o nome da constante sem em maiusculo */
     }
 }
+/*TABELA DE VARIÁVEIS */
+
+/* FAMILIA  -  TIPO PRIMITIVO - WRAPPER CLASS
+ * LÓGICO   -     boolean     -     Boolean  
+ * LITERAIS -       char      -    Character 
+ *          -      string     -     String   
+ * INTEIROS -       byte      -      Byte    
+ *          -      short      -      Short   
+ *          -       int       -     Interger 
+ *          -      long       -      Long    
+ * REAIS    -      float      -      Float   
+ *          -      double     -      Double  
+*/
