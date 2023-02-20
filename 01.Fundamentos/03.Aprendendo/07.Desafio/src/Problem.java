8f109e103d90e0fad7ba72a65d2429cf0eece634
Gabriel Moura Martins da Silva <mmartins.gabriel@yahoo.com>
2023-02-20 17:58:13 -0300

    Projeto Simulador Conta Bancaria

---
+++ 01.Fundamentos/03.Aprendendo/07.Desafio/src/Problem.java
@@ -0,0 +1,14 @@
+public class Problem {
+	public static void main(String[] args) {
+		//Scanner sc = new Scanner(System.in);
+		
+		int A, B, PROD;
+		
+		A = -30;  //sc.nextInt();
+		B = 10; //sc.nextInt();
+		 //TODO: Complete os espaços em branco com uma possível solução para o desafio 
+		PROD = A * B;
+
+		System.out.println(PROD);
+	}
+}
